eee50d2af5561eb6be943a6745fcc0f94c437375
revyrob <kayle.robson@gmail.com>
2023-06-22 23:06:28 -0700

    working on the banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,9 @@ import mountain from '../../assets/mountain.png';
 function Banner() {
   return (
     
-    <section className="h-[600px] bg-cover bg-center max-w-screen-xl m-auto" style={{backgroundImage: `url(${mountain})`}}>
-        <h1>Hello 👋 <br></br>
+    <section className="h-[600px] bg-cover bg-center max-w-screen-xl m-auto flex  items-center pl-[10%]" style={{backgroundImage: `url(${mountain})`}}>
+        <h1 className='text-2xl w-[20rem]'>Hello 👋 <br></br>
+        <br></br>
         My name is Kayle Robson and I am a Front End Web Developer.  </h1>
        
     </section>
