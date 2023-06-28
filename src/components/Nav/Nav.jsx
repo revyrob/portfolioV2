2264f1f2feaefd0e42032201e25153944ed65e5b
revyrob <kayle.robson@gmail.com>
2023-06-28 15:08:19 -0700

    working on nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,36 +1,96 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Stack, Button, Menu, MenuItem, Container, makeStyles } from '@mui/material'
+import { AppBar, Toolbar, IconButton, Stack, Button, Menu, MenuItem, Container, Box  } from '@mui/material'
 import { useState } from 'react'
+import MenuIcon from "@mui/icons-material/Menu";
+
 
 function Nav() {
-    const [anchorEl, setAnchorElNav] = useState(null)
+
+    //problem is that there is only one use state for both my drop down menus.  i need to fix this
+    const [anchorEl, setAnchorEl] = useState(null)
     
     const handleClick = (event) => {
-        setAnchorElNav(event.currentTarget);
+        setAnchorEl(event.currentTarget);
       };
     
       const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
+        setAnchorEl(null);
       };
 
-      
+
 
   return (
     <AppBar position='static' style={{ background: 'rgba(255, 242, 226, .05)' }} className=" max-w-screen-2xl mx-auto">
         <Container maxWidth="xl" className="bg-transparent ">
         <Toolbar >
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              //onClick={handleOpenNavMenu}
+              color="black"
+              //className="hover:text-red-700"
+            > <MenuIcon /></IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                  <a href="/">
+                  
+                     Projects
+                    
+                  </a>
+                </MenuItem>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <a href="/">
+                   
+                     About Me
+                   
+                  </a>
+                </MenuItem>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <a href="/">
+                 
+                    Resume
+                  
+                  </a>
+                </MenuItem>
+                
+         
+                </Menu>
+                </Box>
+
+<Box>
             <Stack direction="row" spacing={2}>
-                <Button className="text-gray-900" id="resources-button" onClick={handleClick}>Projects</Button>
-                <Button className="text-gray-900">About Me</Button>
-                <Button className="text-gray-900">Resume</Button>
+                <Button style={{ color: 'black' }} id="resources-button" onClick={handleClick}>Projects</Button>
+                <Button style={{ color: 'black' }}>About Me</Button>
+                <Button style={{ color: 'black' }}>Resume</Button>
             </Stack>
-            <Menu id="resources-menu" anchorEl={anchorEl} open={Boolean(anchorEl)}
+            {/* <Menu id="resources-menu" anchorEl={anchorEl} open={Boolean(anchorEl)}
               onClose={handleCloseNavMenu}>
                 <MenuItem>Project1</MenuItem>
                 <MenuItem>Project2</MenuItem>
                 <MenuItem>Project3</MenuItem>
 
-            </Menu>
+            </Menu> */}
+            </Box>
         </Toolbar>
         </Container>
     </AppBar>
