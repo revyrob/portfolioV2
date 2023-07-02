ccd9c3bc9bd5a64b342700d0016148de51b104ed
revyrob <kayle.robson@gmail.com>
2023-07-02 15:43:44 -0700

    commented on code

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,18 +6,18 @@ import skillsData from '../../assets/data/skills.json';
 
 function Skills() {
 
-//if it is divisble by two than is will be mapped
+//if it is divisble by two than it is in a small ball
 const evenArray = skillsData.map((key, index) => {
    return ((index % 2 === 0) ? key : null) 
 })
+//if it is NOT divisble by two than it is in a big ball
 const oddArray = skillsData.map((key, index) => {
   return ((index % 2 !== 0) ? key : null) 
 })
+//filt out null from both arrays or objects
 const smArray = evenArray.filter(i => i !== null)
 const bgArray = oddArray.filter(j => j !== null)
 
-console.log(smArray)
-console.log(bgArray)
   return (
     <section className='bg-[#7FBABB] min-h-[400px] w-full py-[2rem] '>
         <div className='bg-white rounded-lg min-h-[350px] w-3/4 m-auto flex justify-center items-center '>
