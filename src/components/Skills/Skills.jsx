e9d82dd775cb65673178cb749b610348a64a7a7c
revyrob <kayle.robson@gmail.com>
2023-07-02 10:26:43 -0700

    adding function for returning every second key in an array of objects

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,18 +6,27 @@ import skillsData from '../../assets/data/skills.json';
 import { useState } from 'react';
 
 function Skills() {
-const [skills, setSkills] = useState(skillsData);
 
-useEffect(() => {
-  setSkills(skills)
-}, []);
+const [skills] = useState(skillsData);
+
+//if it is divisble by two than is will be mapped
+
+// const oddArray = skills.map((key, index) => {
+//   return ((index % 2 !== 0) ? key : oddArray )
+// })
+const twoArray = skills.map((key, index) => {
+  return ((index % 2 === 0) ? key : 0)
+})
+console.log(twoArray)
 
   return (
+
+
     <section className='bg-[#7FBABB] min-h-[400px] w-full py-[2rem]'>
         <div className='bg-white rounded-lg min-h-[350px] w-3/4 m-auto flex justify-center items-center'>
 <img src={ladySkills} alt='working woman with skills' className='invisible object-none object-center lg:visible' />
 
-{skills.map((i) => (
+{twoArray.map((i) => ( 
               <SmBall imgSkill={i.img} imgSkillText={i.skill}/>
             ))}
         <LgBall/>
