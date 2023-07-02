968ec317623cf2b56401e685179b9bd2fdf696a9
revyrob <kayle.robson@gmail.com>
2023-07-02 09:57:05 -0700

    added skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SmBall from '../SmBall/SmBall';
 import LgBall from '../LgBall/LgBall';
 import ladySkills from '../../assets/icons/workLady.png'
@@ -6,7 +6,11 @@ import skillsData from '../../assets/data/skills.json';
 import { useState } from 'react';
 
 function Skills() {
-const [skills] = useState(skillsData) 
+const [skills, setSkills] = useState(skillsData);
+
+useEffect(() => {
+  setSkills(skills)
+}, []);
 
   return (
     <section className='bg-[#7FBABB] min-h-[400px] w-full py-[2rem]'>
