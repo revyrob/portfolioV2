2f1ca88858e31b56a6669d356e3b92483a7ee6fd
revyrob <kayle.robson@gmail.com>
2023-08-29 05:24:08 -0700

    working on the framer motion movement for project section

--- src/components/ProjectSec/ProjectSec.jsx
+++ src/components/ProjectSec/ProjectSec.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import projects from '../../assets/data/projects.json';
 import Projects from '../Projects/Projects';
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function ProjectSec() {
     const listProjects = projects;
+
+      
+  const [ref, inView] = useInView({
+    threshold: 1,
+    triggerOnce: false
+  });
+  
+  const variants = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0.65 }
+  };
+
   return (
     <div className='bg-[#FFF2E2]'>
         <h1 className='text-4xl pt-[3%] flex justify-center items-center font-bold'>Projects</h1>
     {listProjects && listProjects.map((i) => (
-        <Projects key={listProjects.indexOf(i)} bgColor={i.bgColor} name={i.name} info={i.info} projectImg={i.projectImg} skills={i.skills} backend={i.backend} frontend={i.frontend} deployed={i.deployed} id={i.id} infoProject={i.infoProject} infoDevelopment={i.infoDevelopment} infoSolution={i.infoSolution}/>
+      
+        <motion.Projects key={listProjects.indexOf(i)} animate={inView ? "visible" : "hidden"}
+        variants={variants}
+        exit="hidden"
+        transition={{ duration: 2 }}
+        className="box"
+        ref={ref} bgColor={i.bgColor} name={i.name} info={i.info} projectImg={i.projectImg} skills={i.skills} backend={i.backend} frontend={i.frontend} deployed={i.deployed} id={i.id} infoProject={i.infoProject} infoDevelopment={i.infoDevelopment} infoSolution={i.infoSolution}/>
         ))}
+        
+      
         </div>
   )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
-import Skills from '../components/Skills/Skills'
 import Footer from '../components/Footer/Footer'
 import introInfo from '../assets/data/intro.json'
 import ProjectSec from '../components/ProjectSec/ProjectSec'
-// import { useRef, useState } from 'react'
 
 
 function Landing() { 
-  
-  // //useRefs
-  // const containerRef = useRef(null)
-  // const [isVisible, setIsVisible] = useState(false);
-  
-  
-  
-  // useEffect(() => {
-  //   //observe entries
-  //   console.log('myRef', containerRef.current)
-  // }, [])
-  
+
   
   
   return (
@@ -27,7 +14,9 @@ function Landing() {
     <Banner intro1={introInfo.intro[0]} intro2={introInfo.intro[1]}/>
   
     
-   <ProjectSec/>
+   <ProjectSec>
+  
+      </ProjectSec>
       <Footer />
       </div>
       )
